ec1684189afe12ae1a8fc49c553cf33d81362ed3
Albina <axioma.25@gmail.com>
2021-05-31 17:14:15 +0500

    add nuxt,scss

--- javascript/jsresume.js
+++ javascript/jsresume.js
@@ -20,15 +20,14 @@ Vue.component('job-item', {
 			  <div v-else-if="contact.id==3"><i class="fa fa-envelope"></i></div>
               <div v-else><i class="fa fa-map-marker fa-lg"></i></div>
                  <div>  <h4 class="contact-title">{{ contact.title }}</h4>
-				 <span v-if="contact.id==1" class="contact"> 
-				 <a href="tel:+79874991540">{{ contact.value }}</a></span> 
-				 <span v-else-if="contact.id==2" class="contact"> 
-				 <a href="https://wa.me/79874991540">{{ contact.value }}</a></span>
-				<span v-else-if="contact.id==3" class="contact"> 
-				 <a href="https://gmail.com">{{ contact.value }}</a></span>
-  
-				<span v-else class="contact">{{ contact.value }}</span>
-                </div>
+					<span v-if="contact.id==1" class="contact"> 
+					<a href="tel:+79874991540">{{ contact.value }}</a></span> 
+					<span v-else-if="contact.id==2" class="contact"> 
+					<a href="https://wa.me/79874991540">{{ contact.value }}</a></span>
+					<span v-else-if="contact.id==3" class="contact"> 
+					<a href="https://gmail.com">{{ contact.value }}</a></span>
+					<span v-else class="contact">{{ contact.value }}</span>
+                 </div>
             </div>`
 })
 
@@ -39,26 +38,23 @@ Vue.component('duty-item', {
 	  <div class="marginbottom">
 	   <p class="marginbottom">{{duty.value}}</p>
 	  </div>
-   </div> 
-`
+   </div>`
 })
-
 
 Vue.component('education-item', {
 	props: ['education'],
 	template: `
 	<div>
 	  <div class="main-inf-title">
-	   <h3> {{education.title}}</h3>
-	   <div class="years">{{education.year}}</div> 
+	    <h3> {{education.title}}</h3>
+	    <div class="years">{{education.year}}</div> 
       </div>
 	  <div class="main-inf">
 	
 	  <p>{{education.faculty}}</p>
 	  <p class="marginbottom">Специальность: {{education.specialty}}</p>
 	  </div>
-   </div> 
-`
+   </div>`
 })
 
 Vue.component('differences-item', {
@@ -66,14 +62,13 @@ Vue.component('differences-item', {
 	template: `
 	<div>
 	<div class="main-inf-title">
-	 <h3>{{best.title}}</h3>
+	  <h3>{{best.title}}</h3>
 	<div class="years">{{best.year}}</div> 
 	</div>
 	<div class="main-inf">
-	 <p class="marginbottom">{{best.value}}</p>
+	  <p class="marginbottom">{{best.value}}</p>
 	</div>
-  </div>
-	`
+  </div>`
 })
 
 Vue.component('skills-item', {
@@ -81,15 +76,14 @@ Vue.component('skills-item', {
 	template: `
 	<div class="skill" > 
 	<span class="title">{{ skill.title }}</span>
-	
-	<progress class="progressbar" v-if="skill.id==1" max="100" value="70"></progress>
-	<progress class="progressbar" v-if="skill.id==2" max="100" value="60"></progress>
-	<progress class="progressbar" v-if="skill.id==3" max="100" value="40"></progress>
-	<progress class="progressbar"  v-if="skill.id==4" max="100" value="50"></progress>
-	<progress class="progressbar" v-if="skill.id==5" max="100" value="40"></progress>
-	<progress class="progressbar" v-if="skill.id==6" max="100" value="70"></progress>	  
-  </div>   
-	`
+		<progress class="progressbar" v-if="skill.id==1" max="100" value="70"></progress>
+		<progress class="progressbar" v-if="skill.id==2" max="100" value="60"></progress>
+		<progress class="progressbar" v-if="skill.id==3" max="100" value="40"></progress>
+		<progress class="progressbar" v-if="skill.id==4" max="100" value="50"></progress>
+		<progress class="progressbar" v-if="skill.id==5" max="100" value="30"></progress>
+		<progress class="progressbar" v-if="skill.id==6" max="100" value="70"></progress>
+		<progress class="progressbar" v-if="skill.id==7" max="100" value="50"></progress>	  
+  </div>`
 })
 
 let app = new Vue({
@@ -100,18 +94,18 @@ let app = new Vue({
 		image:'foto/avatar.jpg',
 		skills:[
 		{id:1, title:'HTML', value:70},
-		{id:2, title:'CSS', value:60},
+		{id:2, title:'SCSS', value:60},
 		{id:3, title:'JavaScript', value:40},
-		{id:4, title:'English', value:50},
-		{id:5, title: 'Vue.js', value:40},
-		{id:6, title: 'GIT', value:70}
+		{id:4, title:'Vue.js', value:50},
+		{id:5, title: 'Nuxt.js', value:30},
+		{id:6, title: 'GIT', value:70},
+		{id:7, title:'English', value:50},
 	    ],
 	  	contacts:[
 		{id:1, title:'телефон', value:'8-987-499-15-40'},
 		{id:2, title:'WhatsApp', value:'8-987-499-15-40'},
 		{id:3, title:'email', value:'axioma.25@gmail.com'},
         {id:4, title:'адрес', value:'Уфа, ул. Зои Космодемьянской, д.60'},
-		
         ],
 		duties: [
 		{id:1, value:'Разработка сайтов для клиентов в команде программистов, их последующая поддержка.'}	
@@ -136,8 +130,8 @@ let app = new Vue({
 			faculty:'Факультет информатики и робототехники',
 			specialty:'Автоматизированные системы обработки информации и управления',
 			year:'2003-2008'
-		}
-	],
+			}
+		],
 		differences:[
 			{title:'УГАТУ', year: '2008', 
 			value: 'Победитель в студенческой научно-теоретической конференции "Неделя науки"'},
